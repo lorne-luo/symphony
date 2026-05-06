14ff48ea24e58255e96da6454e1ba37734d0b4e0
Lorne <dev@luotao.net>
2026-05-06 13:08:52 +1000

    feat(tracker): dispatch to Jira.Adapter when tracker.kind = jira

--- elixir/lib/symphony_elixir/tracker.ex
+++ elixir/lib/symphony_elixir/tracker.ex
@@ -40,7 +40,8 @@ defmodule SymphonyElixir.Tracker do
   def adapter do
     case Config.settings!().tracker.kind do
       "memory" -> SymphonyElixir.Tracker.Memory
-      _ -> SymphonyElixir.Linear.Adapter
+      "jira"   -> SymphonyElixir.Jira.Adapter
+      _        -> SymphonyElixir.Linear.Adapter
     end
   end
 end
